6b61bf18cdfc34bfb102bbb1095950df33e76a14
Thomas Lovén <thomasloven@gmail.com>
2020-10-21 20:34:01 +0200

    Fix race condition on boot. Fix #106

--- custom_components/browser_mod/__init__.py
+++ custom_components/browser_mod/__init__.py
@@ -3,21 +3,27 @@ import logging
 from .mod_view import setup_view
 from .connection import setup_connection
 from .service import setup_service
-from .const import DOMAIN, DATA_DEVICES, DATA_ALIASES, DATA_ADDERS, CONFIG_DEVICES, DATA_CONFIG
-
+from .const import (
+    DOMAIN,
+    DATA_DEVICES,
+    DATA_ALIASES,
+    DATA_ADDERS,
+    CONFIG_DEVICES,
+    DATA_CONFIG
+)
 
 _LOGGER = logging.getLogger(__name__)
 
+
 async def async_setup(hass, config):
 
-    await setup_connection(hass, config)
     setup_view(hass)
 
     aliases = {}
     for d in config[DOMAIN].get(CONFIG_DEVICES, {}):
         name = config[DOMAIN][CONFIG_DEVICES][d].get("name", None)
         if name:
-            aliases[name] = d.replace('_','-')
+            aliases[name] = d.replace('_', '-')
 
     hass.data[DOMAIN] = {
         DATA_DEVICES: {},
@@ -26,16 +32,14 @@ async def async_setup(hass, config):
         DATA_CONFIG: config[DOMAIN],
         }
 
-    await hass.helpers.discovery.async_load_platform("media_player", DOMAIN, {}, config)
-    await hass.helpers.discovery.async_load_platform("sensor", DOMAIN, {}, config)
-    await hass.helpers.discovery.async_load_platform("binary_sensor", DOMAIN, {}, config)
-    await hass.helpers.discovery.async_load_platform("light", DOMAIN, {}, config)
-    await hass.helpers.discovery.async_load_platform("camera", DOMAIN, {}, config)
-
+    async_load_platform = hass.helpers.discovery.async_load_platform
+    await async_load_platform("media_player", DOMAIN, {}, config)
+    await async_load_platform("sensor", DOMAIN, {}, config)
+    await async_load_platform("binary_sensor", DOMAIN, {}, config)
+    await async_load_platform("light", DOMAIN, {}, config)
+    await async_load_platform("camera", DOMAIN, {}, config)
 
     await setup_service(hass)
+    await setup_connection(hass, config)
 
     return True
-
-
-
