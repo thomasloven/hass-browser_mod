fe1a180e4fdd2c2a6705d2f6ef1eba6c9443f716
Thomas Lovén <thomasloven@gmail.com>
2021-02-09 23:26:28 +0100

    Add service descriptions

--- custom_components/browser_mod/__init__.py
+++ custom_components/browser_mod/__init__.py
@@ -28,8 +28,6 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass, config):
 
-    _LOGGER.error(hass.config_entries)
-
     if not hass.config_entries.async_entries(DOMAIN):
         hass.async_create_task(
             hass.config_entries.flow.async_init(
