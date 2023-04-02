d06d036328d18511705123f342c6108b5796d437
Pulpyyyy <32004297+Pulpyyyy@users.noreply.github.com>
2023-04-02 20:55:40 +0200

    typo

--- custom_components/browser_mod/browser_mod_panel.js
+++ custom_components/browser_mod/browser_mod_panel.js
@@ -366,7 +366,7 @@ function e(e,t,i,o){var s,n=arguments.length,r=n<3?t:null===o?o=Object.getOwnPro
             ></browser-mod-settings-table>
           </ha-expansion-panel>
           <ha-expansion-panel
-            .header=${"Hide ineraction icon"}
+            .header=${"Hide interaction icon"}
             .secondary=${"Hide the icon showing that Browser Mod will not work fully until interacted with"}
             leftChevron
           >
