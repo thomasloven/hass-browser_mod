35029c0694bfef7b6180f760f19640253bd73af4
dcapslock <dcapes-davis@outlook.com.au>
2025-03-16 12:08:36 +1100

    Allow User Targets

--- custom_components/browser_mod/service.py
+++ custom_components/browser_mod/service.py
@@ -1,6 +1,7 @@
 import logging
 
 from homeassistant.helpers import device_registry
+from homeassistant.helpers import template
 
 from .const import (
     BROWSER_MOD_SERVICES,
@@ -13,17 +14,27 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_services(hass):
     def call_service(service, targets, data):
+        browserTargets = targets["browsers"]
+        userTargets = targets["users"]
 
         browsers = hass.data[DOMAIN][DATA_BROWSERS]
 
         # If no targets were specified, send to all browsers
-        if targets is None:
-            targets = browsers.keys()
+        if browserTargets is None and userTargets is None:
+            browserTargets = browsers.keys()
+        else:
+            browserTargets = []
 
-        for target in targets:
-            if target not in browsers:
+        if len(userTargets):
+            for userId in userTargets:
+                for key, browser in browsers.items():
+                    if browser.data.get("browser", {}).get("userData", {}).get("id") == userId:
+                        browserTargets.append(key)
+
+        for browserTarget in browserTargets:
+            if browserTarget not in browsers:
                 continue
-            browser = browsers[target]
+            browser = browsers[browserTarget]
             hass.create_task(browser.send(service, **data))
 
     def handle_service(call):
@@ -53,7 +64,22 @@ async def async_setup_services(hass):
 
             browserIDs = set(browserIDs)
 
-        call_service(service, browserIDs, data)
+        # Support User Targets
+        users = data.pop("user_id", [])
+        if isinstance(users, str):
+            users = [users]
+        
+        userIDs = None
+        if len(users):
+            userIDs = set()
+            for user in users:
+                userId = template.state_attr(hass, user, "user_id")
+                if userId:
+                    userIDs.add(userId)
+
+        targets = {"browsers": browserIDs, "users": userIDs}
+
+        call_service(service, targets, data)
 
     for service in BROWSER_MOD_SERVICES:
         hass.services.async_register(DOMAIN, service, handle_service)
