thomasloven/hass-browser_mod

commit ef1c5b778fb6184ea42b5ed53d3e594a8756536a

Revamped browser_mod (re)connection to support HASS restarts (#862)

js/plugin/connection.ts
@@ -4,9 +4,11 @@ export const ConnectionMixin = (SuperClass) => {
   class BrowserModConnection extends SuperClass {
     public hass;
     public connection;
+    public ready = false;
+
     private _data;
-    public connected = false;
     private _connectionResolve;
+
     public connectionPromise = new Promise((resolve) => {
       this._connectionResolve = resolve;
     });
@@ -23,19 +25,47 @@ export const ConnectionMixin = (SuperClass) => {
       });
     }
 
-    private fireEvent(event, detail = undefined) {
+    // Propagate internal browser event
+    private fireBrowserEvent(event, detail = undefined) {
       this.dispatchEvent(new CustomEvent(event, { detail, bubbles: true }));
     }
 
+    /*
+     * Main state flags explained:
+     * * `connected` and `disconnected` refers to WS connection,
+     * * `ready` refers to established communication between browser and component counterpart.
+     */
+
+    // Component and frontend are mutually ready
+    private onReady = () => {
+      this.ready = true;
+      this.fireBrowserEvent("browser-mod-ready");
+      window.setTimeout(() => this.sendUpdate({}), 1000);
+    }
+
+    // WebSocket has disconnected
+    private onDisconnected = () => {
+      this.ready = false;
+      this.fireBrowserEvent("browser-mod-disconnected");
+    }
+
+    // Handle incoming message
     private incoming_message(msg) {
+      // Check for readiness (of component and browser)
+      if (!this.ready) {
+        this.LOG("Integration ready: WebSocket connected and browser_mod loaded");
+        this.onReady();
+      }
+      // Handle messages
       if (msg.command) {
         this.LOG("Command:", msg);
-        this.fireEvent(`command-${msg.command}`, msg);
+        this.fireBrowserEvent(`command-${msg.command}`, msg);
       } else if (msg.browserEntities) {
         this.browserEntities = msg.browserEntities;
       } else if (msg.result) {
         this.update_config(msg.result);
       }
+      // Resolve first connection promise
       this._connectionResolve?.();
       this._connectionResolve = undefined;
     }
@@ -52,12 +82,7 @@ export const ConnectionMixin = (SuperClass) => {
       if (!this.registered && this.global_settings["autoRegister"] === true)
         this.registered = true;
 
-      if (!this.connected) {
-        this.connected = true;
-        this.fireEvent("browser-mod-connected");
-      }
-
-      this.fireEvent("browser-mod-config-update");
+      this.fireBrowserEvent("browser-mod-config-update");
 
       if (update) this.sendUpdate({});
     }
@@ -66,32 +91,38 @@ export const ConnectionMixin = (SuperClass) => {
       const conn = (await hass()).connection;
       this.connection = conn;
 
-      // Subscribe to configuration updates
-      conn.subscribeMessage((msg) => this.incoming_message(msg), {
-        type: "browser_mod/connect",
-        browserID: this.browserID,
-      });
+      const connectBrowserModComponent = () => {
+        this.LOG("Subscribing to browser_mod/connect events");
+        conn.subscribeMessage((msg) => this.incoming_message(msg), {
+          type: "browser_mod/connect",
+          browserID: this.browserID,
+        });
+      };
+
+      // Initial connect component subscription
+      connectBrowserModComponent();
+      // If this fails, then:
+      // Observe `component_loaded` to track when `browser_mod` is dynamically added
+      conn.subscribeEvents((haEvent) => {
+        if (haEvent.data?.component === "browser_mod") {
+          this.LOG("Detected browser_mod component load");
+          connectBrowserModComponent();
+        }
+      }, "component_loaded");
 
       // Keep connection status up to date
-      conn.addEventListener("disconnected", () => {
-        this.connected = false;
-        this.fireEvent("browser-mod-disconnected");
-      });
       conn.addEventListener("ready", () => {
-        this.connected = true;
-        this.fireEvent("browser-mod-connected");
-        this.sendUpdate({});
+        this.onConnected();
       });
-
+      conn.addEventListener("disconnected", () => {
+        this.onDisconnected();
+      });
       window.addEventListener("connection-status", (ev: CustomEvent) => {
         if (ev.detail === "connected") {
-          this.connected = true;
-          this.fireEvent("browser-mod-connected");
-          window.setTimeout(() => this.sendUpdate({}), 1000);
+          this.onConnected();
         }
         if (ev.detail === "disconnected") {
-          this.connected = false;
-          this.fireEvent("browser-mod-disconnected");
+          this.onDisconnected();
         }
       });
 
@@ -214,7 +245,7 @@ export const ConnectionMixin = (SuperClass) => {
     }
 
     sendUpdate(data) {
-      if (!this.connected || !this.registered) return;
+      if (!this.ready || !this.registered) return;
 
       const dt = new Date();
       this.LOG("Send:", data);
@@ -227,7 +258,7 @@ export const ConnectionMixin = (SuperClass) => {
     }
 
     browserIDChanged(oldID, newID) {
-      this.fireEvent("browser-mod-config-update");
+      this.fireBrowserEvent("browser-mod-config-update");
 
       if (
         this.browsers?.[oldID] !== undefined &&

js/config_panel/browser-settings-card.ts
@@ -6,7 +6,7 @@ class BrowserModRegisteredBrowsersCard extends LitElement {
   @property() dirty = false;
 
   toggleRegister() {
-    if (!window.browser_mod?.connected) return;
+    if (!window.browser_mod?.ready) return;
     window.browser_mod.registered = !window.browser_mod.registered;
     this.dirty = true;
   }
@@ -30,7 +30,7 @@ class BrowserModRegisteredBrowsersCard extends LitElement {
       <ha-card outlined>
         <h1 class="card-header">
           <div class="name">This Browser</div>
-          ${window.browser_mod?.connected
+          ${window.browser_mod?.ready
             ? html`
                 <ha-icon
                   class="icon"

js/plugin/activity.ts
@@ -11,7 +11,7 @@ export const ActivityMixin = (SuperClass) => {
       this.addEventListener("fully-update", () => {
         this.activityTrigger();
       });
-      this.addEventListener("browser-mod-connected", () =>
+      this.addEventListener("browser-mod-ready", () =>
         this._activity_state_update()
       );
     }
@@ -28,7 +28,7 @@ export const ActivityMixin = (SuperClass) => {
       }
       this.activityTriggered = true;
       if (touched) {
-        this.fireEvent("browser-mod-activity");
+        this.fireBrowserEvent("browser-mod-activity");
       }
       clearTimeout(this._activityTimeout);
       this._activityTimeout = setTimeout(

js/plugin/browser-player.ts
@@ -51,7 +51,7 @@ class BrowserPlayer extends LitElement {
     window.browser_mod?._video_player?.addEventListener(event, () =>
       this.requestUpdate()
     );
-    window.browser_mod?.addEventListener("browser-mod-connected", () =>
+    window.browser_mod?.addEventListener("browser-mod-ready", () =>
       this._reconnect()
     );
   }

js/plugin/browser.ts
@@ -19,7 +19,7 @@ export const BrowserStateMixin = (SuperClass) => {
       );
 
       this.addEventListener("fully-update", () => this._browser_state_update());
-      this.addEventListener("browser-mod-connected", () =>
+      this.addEventListener("browser-mod-ready", () =>
         this._browser_state_update()
       );
 

js/plugin/camera.ts
@@ -60,7 +60,7 @@ export const CameraMixin = (SuperClass) => {
         if (e.name !== "NotAllowedError") throw e;
         else {
           this.cameraError = true;
-          this.fireEvent("browser-mod-config-update");
+          this.fireBrowserEvent("browser-mod-config-update");
         }
       }
     }

js/plugin/fullyKiosk.ts
@@ -120,7 +120,7 @@ export const FullyMixin = (C) => {
     }
 
     fullyEvent(event = undefined) {
-      this.fireEvent("fully-update", { event });
+      this.fireBrowserEvent("fully-update", { event });
     }
   };
 };

js/plugin/mediaPlayer.ts
@@ -79,7 +79,7 @@ export const MediaPlayerMixin = (SuperClass) => {
         this._player_update();
       });
 
-      this.addEventListener("browser-mod-connected", () =>
+      this.addEventListener("browser-mod-ready", () =>
         this._player_update()
       );
 

commit 8e06d3ccd134b0a03cfc7898f297e0ddd492500b

Fix for issue introduced with PR #862

js/plugin/connection.ts
@@ -112,14 +112,14 @@ export const ConnectionMixin = (SuperClass) => {
 
       // Keep connection status up to date
       conn.addEventListener("ready", () => {
-        this.onConnected();
+        this.onReady();
       });
       conn.addEventListener("disconnected", () => {
         this.onDisconnected();
       });
       window.addEventListener("connection-status", (ev: CustomEvent) => {
         if (ev.detail === "connected") {
-          this.onConnected();
+          this.onReady();
         }
         if (ev.detail === "disconnected") {
           this.onDisconnected();