8c880f4d9bb797d7d9e23bbedc5ab42662f3c4ca
dcapslock <dcapes-davis@outlook.com.au>
2025-06-23 18:05:03 +1000

    Update js/config_panel/browser-settings-card.ts
    Co-authored-by: Thomas Lovén <thomasloven@gmail.com>

--- js/config_panel/browser-settings-card.ts
+++ js/config_panel/browser-settings-card.ts
@@ -101,7 +101,7 @@ class BrowserModRegisteredBrowsersCard extends LitElement {
                 ${window.browser_mod?.cameraError
                   ? html`
                       <ha-alert alert-type="error">
-                        Setting up the device camera failed. Make sure you are browsering
+                        Setting up the device camera failed. Make sure you are browsing
                         in a secure (https://) context and have
                         allowed use of the camera in your browser.
                       </ha-alert>
