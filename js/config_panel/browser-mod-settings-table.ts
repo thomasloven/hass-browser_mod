f8a815485a13561215bd0307b6fe75db0fc0e7c3
dcapslock <dcapes-davis@outlook.com.au>
2026-01-19 10:14:33 +1100

    Refactor settings table update logic ti fix ongoing issues

--- js/config_panel/browser-mod-settings-table.ts
+++ js/config_panel/browser-mod-settings-table.ts
@@ -1,7 +1,7 @@
 import { LitElement, html, css, nothing, PropertyValues } from "lit";
 import { until } from 'lit/directives/until.js';
 import { property } from "lit/decorators.js";
-import { debounce, selectTree } from "../helpers";
+import { compare_deep, debounce, selectTree } from "../helpers";
 
 let _users = undefined;
 
@@ -16,8 +16,6 @@ class BrowserModSettingsTable extends LitElement {
 
   @property() tableData = <any>[];
 
-  private _firstUpdatedTimestamp = 0;
-
   private _tableFirstUpdate: (() => void) | null = null;
   private _tableUpdate = new Promise<void>((resolve) => {
     this._tableFirstUpdate = resolve;
@@ -25,40 +23,47 @@ class BrowserModSettingsTable extends LitElement {
 
   firstUpdated() {
     window.browser_mod.addEventListener("browser-mod-config-update", () => {
-      debounce(() => this.updateTable(), 1000)();
+      this.updateTableDebounced();
     }
     );
-    this._firstUpdatedTimestamp = Date.now();
   }
 
   protected shouldUpdate(changedProperties: PropertyValues): boolean {
-    if ((changedProperties).has("settingKey") || changedProperties.has("tableData")) {
+    if ((changedProperties).has("settingKey")) {
+      this.settingKey == "titleTemplate" && console.log("BrowserModSettingsTable(titleTemplate): shouldUpdate(): settingKey changed, should update", changedProperties, this.settingKey);
       return true;
-    } else if (
-      changedProperties.has("hass") &&
-      changedProperties.get("hass") !== undefined && this.hass !== undefined
-    ) {
-      // ha-data-table needs initial update to render virtualizer correctly (??) 
-      // but then we can skip further updates on hass changes so not to force re-renders
-      // which will flicker the table
-      return Date.now() - this._firstUpdatedTimestamp < 1000;
+    } else if (changedProperties.has("tableData")) {
+      const oldTableData = changedProperties.get("tableData");
+      if (oldTableData === undefined && this.tableData !== undefined && this.tableData.length > 0) {
+        this.settingKey == "titleTemplate" && console.log("BrowserModSettingsTable(titleTemplate): shouldUpdate(): tableData was undefined or empty, should update", changedProperties, this.tableData);
+        return true;
+      } else if (Array.isArray(oldTableData) && Array.isArray(this.tableData)) {
+        if (!compare_deep(oldTableData, this.tableData)) {
+          this.settingKey == "titleTemplate" && console.log("BrowserModSettingsTable(titleTemplate): shouldUpdate(): tableData changed, should update", changedProperties, this.tableData);
+          return true;
+        }
+      }
     }
+    this.settingKey == "titleTemplate" && console.log("BrowserModSettingsTable(titleTemplate): shouldUpdate(): should not update", changedProperties);
     return false;
   }
 
   updated(changedProperties) {
     if (changedProperties.has("settingKey")) {
-      this.updateTable();
+      this.settingKey == "titleTemplate" && console.log("BrowserModSettingsTable(titleTemplate): updated(): settingKey changed, updating table", changedProperties, this.settingKey);
+      this.updateTableDebounced();
     } else if (
       changedProperties.has("hass") &&
       changedProperties.get("hass") === undefined
-    )
-      this.updateTable();
+    ) {
+      this.settingKey == "titleTemplate" && console.log("BrowserModSettingsTable(titleTemplate): updated(): hass initialized, updating table", changedProperties, this.hass);
+      this.updateTableDebounced();
+    }
   }
 
   async fetchUsers(): Promise<any[]> {
     if (_users === undefined)
-      _users = await this.hass.callWS({ type: "config/auth/list" });
+      _users = this.hass.callWS({ type: "config/auth/list" });
     return _users;
   }
 
@@ -258,6 +263,8 @@ class BrowserModSettingsTable extends LitElement {
       }
     );
   }
+
+  updateTableDebounced = debounce(() => this.updateTable(), 1000);
 
   async updateTable() {
     if (this.hass === undefined) return;
